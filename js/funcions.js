3640de81022ac6b249638cc2ebf39e9cbfa7abc6
HectorGC <hectorcde21@gmail.com>
2024-09-25 12:30:12 +0200

    commitp2

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,8 @@
 function confirmaRegistre(){
     //completa
+    alert("registrant estudiant")
+    console.log("registrant estudiant");
+    let tagdiv = document.getElementById('formDiv')
+    tagdiv.innerHTML = "<p class='important'>Registrat</p>"
+    return false
 }
